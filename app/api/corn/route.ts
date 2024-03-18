5f9c9f8e65dae484efe93400981c9c08f4e1860d
Avinash <avinashchowdhury81@gmail.com>
2024-03-18 22:23:57 +0530

    updated

--- app/api/corn/route.ts
+++ app/api/corn/route.ts
@@ -9,7 +9,7 @@ export const maxDuration = 10; // This function can run for a maximum of 300 sec
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
     connectToDB();
 
